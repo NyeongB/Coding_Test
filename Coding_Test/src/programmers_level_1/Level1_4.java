4659166126d6d973f1169e6e57cd0b2711fceb2f
NyeongB <kdtoaj14@ajou.ac.kr>
2020-08-20 16:17:16 +0900

    프로그래머스 레벨1 약수의 합

--- Coding_Test/src/programmers_level_1/Level1_4.java
+++ Coding_Test/src/programmers_level_1/Level1_4.java
@@ -18,12 +18,19 @@ class Solution1_4 {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=1; i<=n; i++)
+        
+        //n을 n/2 말고 약수 값으로 바꾸면 보다 효율적으로 고칠 수 있습니다.
+        //포문 돌아가는 숫자를에서 절반 이하로 떨어트릴 수 있어요.
+        // 맨 마지막에 n을 더해줌 절반까지 돌리면 n을 안더해주기때문에
+        
+
+
+        for(int i=1; i<=n/2; i++)
         {
             if(n%i==0)
                 answer += i;
         }
         
-        return answer;
+        return answer+n;
     }
 }
