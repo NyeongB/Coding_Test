3f5c3ccfa3f8b9b6560c70eeb6317d1071f7bee9
NyeongB <kdtoaj14@ajou.ac.kr>
2020-08-27 13:54:23 +0900

    프로그래머스 레벨2 H-index

---
+++ Coding_Test/src/programmers_level_2/Level2_5.java
@@ -0,0 +1,40 @@
+// Date : 2020.08.27
+// Title : H-Index
+// Author : Choi Cheol Nyeong
+// Language : Java
+ 
+package programmers_level_2;
+
+import java.util.Arrays;
+
+public class Level2_5
+{
+	public static void main(String[] args)
+	{
+		Solution2_5 solution2_5 = new Solution2_5();
+		System.out.println(solution2_5.solution(new int[] {3,0,6,1,5}));
+	}
+	
+}
+
+
+class Solution2_5 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        int n = citations.length;
+        int h = 0;
+        
+        for(int i=0; i<n; i++)
+        {
+            h = n - i;
+            if(citations[i]>=h)
+            {
+                answer = h;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
