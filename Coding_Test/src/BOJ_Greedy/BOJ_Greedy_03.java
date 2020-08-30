90e9f1567709927c9a508830a26b4976c4cb9050
NyeongB <kdtoaj14@ajou.ac.kr>
2020-08-30 21:33:51 +0900

    백준 그리디 잃어버린 괄호

---
+++ Coding_Test/src/BOJ_Greedy/BOJ_Greedy_03.java
@@ -0,0 +1,90 @@
+// Date : 2020.08.30
+// Title : 잃어버린 괄호
+// Author : Choi Cheol Nyeong
+// Language : Java
+// classification : Greedy
+ 
+package BOJ_Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_Greedy_03
+{
+	public static void main(String[] args) throws IOException
+	{
+		//55-50+40 min -> -35
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String [] str = br.readLine().split("\\-");
+		
+		int answer = 0;
+		
+		for(int i=0; i<str.length; i++)
+		{	
+			
+			if(i==0)
+				answer += plus(str[i]);
+			else
+			{
+				answer -=plus(str[i]);
+			}
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static int plus(String s)
+	{
+		int result = 0;
+		
+		String [] temp = s.split("\\+");
+		result = Integer.parseInt(temp[0]);
+		for(int i=1; i<temp.length; i++)
+			result += Integer.parseInt(temp[i]);
+		
+		return result;
+	}
+	
+}
+
+
+/*
+
+초기에 한 방식
+이방법으로 하면 백준에선 자꾸 런타임 에러가 뜬다.. 
+30분째 해멘결과 컴파일러 차이같다는 결론을 내렸다. 
+첫 문자가 0 으로 시작한것때문인가 
+왜 메소드로 만들어서 하면 되는거지.. 
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//55-50+40 min -> -35
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String [] str = sc.nextLine().split("-");
+		
+		int answer = 0;
+		answer += Integer.parseInt(str[0]);
+		for(int i=1; i<str.length; i++)
+		{	
+			//System.out.println(str[i]);
+			String [] temp = str[i].split("\\+");
+			
+			for(int j=0; j<temp.length; j++)
+				answer-=Integer.parseInt(temp[j]);
+			
+			
+		}
+		
+		System.out.println(answer);
+	}
+	
+}
+*/
