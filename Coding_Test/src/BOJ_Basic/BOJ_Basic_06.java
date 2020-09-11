2c0b6fcb5c7af4eb30b2083dd38f2acb406f86c0
NyeongB <kdtoaj14@ajou.ac.kr>
2020-09-12 00:14:26 +0900

    백준 기본 초콜릿 자르기

---
+++ Coding_Test/src/BOJ_Basic/BOJ_Basic_06.java
@@ -0,0 +1,24 @@
+// Date : 2020.09.12
+// Title : 초콜릿 자르기
+// Author : Choi Cheol Nyeong
+// Language : Java
+
+package BOJ_Basic;
+
+import java.util.Scanner;
+
+public class BOJ_Basic_06
+{
+	public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        System.out.println(n * m - 1);
+    }
+}
+// 문제를 풀면서 연습장에 그려보며 규칙을 찾아보았다
+// 설마했는데 맞는 규칙이였다.. 
+// 기본의 기본문제는 안푸는걸로한다.. 
