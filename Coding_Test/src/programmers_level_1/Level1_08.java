2e8f36f71445bf5a425d263ca890dc9140f9322f
NyeongB <kdtoaj14@ajou.ac.kr>
2020-09-15 00:13:37 +0900

    프로그래머스 레벨1 시저 암호(R)

--- Coding_Test/src/programmers_level_1/Level1_08.java
+++ Coding_Test/src/programmers_level_1/Level1_08.java
@@ -9,11 +9,38 @@ public class Level1_08
 	public static void main(String[] args)
 	{
 		Solution1_8 solution1_8 = new Solution1_8();
-		System.out.println(solution1_8.solution("b c B F d", 26));
+		System.out.println(solution1_8.solution("b c B F d", 25));
 	}
 	
 }
 
+class Solution1_8 {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            char temp = s.charAt(i);
+            
+            if(65<= temp && temp <= 90)
+            {
+                temp = (char)((temp-65+n)%26+65);
+            }
+            else if(Character.isLowerCase(temp))
+            {
+                temp = (char)((temp-97+n)%26+97);
+            }
+            
+            answer += temp;
+            
+        }
+        
+        return answer;
+    }
+}
+
+/*
 class Solution1_8 {
     public String solution(String s, int n) {
         String answer = "";
@@ -42,3 +69,4 @@ class Solution1_8 {
     }
    
 }
+*/
