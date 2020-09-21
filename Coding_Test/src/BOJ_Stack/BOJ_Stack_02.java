bb574140c2e6e9c9ccc3832f4a52d7a726155417
NyeongB <kdtoaj14@ajou.ac.kr>
2020-09-21 22:19:32 +0900

    백준 스택 괄호(R)

--- Coding_Test/src/BOJ_Stack/BOJ_Stack_02.java
+++ Coding_Test/src/BOJ_Stack/BOJ_Stack_02.java
@@ -1,4 +1,4 @@
-// Date : 2020.08.30
+// Date : 2020.09.21
 // Title : 괄호
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -6,12 +6,70 @@
 
 package BOJ_Stack;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class BOJ_Stack_02
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		Stack<Character> st;
+		
+		while(n-- >0)
+		{
+			st = new Stack<Character>();
+			
+			String str = br.readLine();
+			
+			for(int i=0; i<str.length(); i++)
+			{
+				if(str.charAt(i)=='(')
+				{
+					st.push('(');
+				}
+				else
+				{
+					if(!st.isEmpty())
+					{
+						st.pop();
+					}
+					else
+					{
+						st.push(')');	// 사실 아무거나 넣어도 상관없음 비어있지 않음의 조건검사 용도
+						break;
+					}
+				}
+			}
+			
+			if(st.isEmpty())
+			{
+				sb.append("YES\n");
+			}
+			else
+			{
+				sb.append("NO\n");
+			}
+			
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
+// 기본 스택 문제 
+// 스택의 감이 떨어지지 않도록 다시 풀어보았다.
+// 많이 익숙해진듯하다.
+
+/*
+public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		
@@ -56,5 +114,4 @@ public class BOJ_Stack_02
 		System.out.println(sb.toString());
 		
 	}
-
-}
+*/
