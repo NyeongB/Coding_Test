18bfd25ed0d186361b67ad4311289bbf1262a0f7
NyeongB <kdtoaj14@ajou.ac.kr>
2020-09-22 17:27:05 +0900

    백준 그리디 설탕배달(R)

--- Coding_Test/src/BOJ_Greedy/BOJ_Greedy_05.java
+++ Coding_Test/src/BOJ_Greedy/BOJ_Greedy_05.java
@@ -1,4 +1,4 @@
-// Date : 2020.09.03
+// Date : 2020.09.22
 // Title : 설탕 배달
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -6,14 +6,52 @@
  
 package BOJ_Greedy;
 
-
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ_Greedy_05
 {
 	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n%5==0)
+		{
+			System.out.println(n/5);
+			return;
+		}
+		else
+		{
+			int temp = n / 5;
+			for(int i=temp; i>0; i--)
+			{
+				if((n - 5*i)%3 ==0)
+				{
+					System.out.println(i+(n - 5*i)/3);
+					return;
+				}
+			}
+		}
+		
+		if(n%3==0)
+		{
+			System.out.println(n/3);
+		}
+		else
+			System.out.println(-1);
+		
+	}
+	
+}
+// 그리디 기본 문제
+// 중요한것은 가장큰 5로 나눈것, 5와 3을 섞는것중에서 5가 가장 많이 있는 케이스부터 보는것, 나머진 3으로만 이루어진것이며
+// 또 그나머지는 구할수없는 값으로 인지하는것이 가장 중요하다.
+
+/*
+public static void main(String[] args) throws IOException
 	{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,5 +86,4 @@ public class BOJ_Greedy_05
 		
 		
 	}
-	
-}
+*/
