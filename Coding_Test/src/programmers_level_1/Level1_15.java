6e35f36eb1c4d2df272bc0c6ff02dba7758a7d5e
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-07 17:26:32 +0900

    프로그래머스 자리수 더하기(R)

--- Coding_Test/src/programmers_level_1/Level1_15.java
+++ Coding_Test/src/programmers_level_1/Level1_15.java
@@ -1,8 +1,7 @@
-
-// Date : 2020.08.20
+// Date : 2020.08.20, 20.11.07
 // Title : 자릿수 더하기
 // Author : Choi Cheol Nyeong
- 
+
 package programmers_level_1;
 
 public class Level1_15
@@ -12,20 +11,25 @@ public class Level1_15
 		Solution1_15 solution1_15 = new Solution1_15();
 		System.out.println(solution1_15.solution(129));
 	}
-	
+
 }
 
-class Solution1_15 {
-    public int solution(int n) {
-        int answer = 0;
+class Solution1_15
+{
 
-        String num = "" + n;
-        
-        for(int i=0; i<num.length(); i++)
-        {
-            answer += num.charAt(i)-'0';
-        }
+	public int solution(int n)
+	{
+		int answer = 0;
 
-        return answer;
-    }
+		while (n > 0)
+		{
+			answer += n % 10;
+			n /= 10;
+		}
+
+		return answer;
+	}
 }
+
+// 기존 코드가 7.08ms 걸린다면 이코드는 0.02 ms 걸린다.
+// 문제를 푸는것만이 능사가 아닌 코드 효율을 본다
