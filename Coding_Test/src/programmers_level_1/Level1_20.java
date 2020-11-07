7c481433dbefb07cbc8bc6084a69cfa02010a090
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-07 18:08:49 +0900

    프로그래머스 자연수 뒤집어 배열(R)

--- Coding_Test/src/programmers_level_1/Level1_20.java
+++ Coding_Test/src/programmers_level_1/Level1_20.java
@@ -1,17 +1,19 @@
-// Date : 2020.08.24
+// Date : 2020.08.24 20.11.07
 // Title : 자연수 뒤집어 배열로 만들기
 // Author : Choi Cheol Nyeong
 
 package programmers_level_1;
+
+import java.util.ArrayList;
 
 public class Level1_20
 {
 	public static void main(String[] args)
 	{
 		Solution1_20 solution1_20 = new Solution1_20();
-		int [] arr = solution1_20.solution(12345);
-		
-		for(int i=0; i<arr.length; i++)
+		int[] arr = solution1_20.solution(12345);
+
+		for (int i = 0; i < arr.length; i++)
 			System.out.print(arr[i]);
 	}
 
@@ -21,14 +23,27 @@ class Solution1_20
 {
 	public int[] solution(long n)
 	{
-		String str = "" + n;
-		int num = str.length();
-		int[] answer = new int[num];
-		int index = 0;
-		for (int i = num - 1; i >= 0; i--)
+		int[] answer =
+		{};
+
+		ArrayList<Long> list = new ArrayList<>();
+
+		while (n > 0)
 		{
-			answer[index++] = str.charAt(i) - '0';
+			list.add(n % 10);
+			n /= 10;
 		}
+
+		answer = new int[list.size()];
+
+		int index = 0;
+		
+		for (long num : list)
+		{
+			answer[index++] = (int) num;
+		}
+
 		return answer;
 	}
 }
+// 기존 코드와 비교도 안되게 빠르다
