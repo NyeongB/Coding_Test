c3a3534c6c0b69e20b679cc295c9dd273e07b7a8
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-08 18:00:10 +0900

    백준 스택 쇠막대기(R)

--- Coding_Test/src/BOJ_Stack/BOJ_Stack_01.java
+++ Coding_Test/src/BOJ_Stack/BOJ_Stack_01.java
@@ -1,4 +1,4 @@
-// Date : 2020.08.30, 20.09.25
+// Date : 2020.08.30, 20.09.25, 20.11.08
 // Title : 쇠막대기
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -15,37 +15,41 @@ public class BOJ_Stack_01
 {
 	public static void main(String[] args) throws IOException
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
 		
-		Stack<Character> st = new Stack<Character>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char [] arr = br.readLine().toCharArray();
 		
 		int answer = 0;
 		
-		for(int i=0; i<str.length(); i++)
+		Stack<Character> st = new Stack<Character>();
+		
+		for(int i=0; i<arr.length; i++)
 		{
-			char temp = str.charAt(i);
+			char temp = arr[i];
+			
 			if(temp=='(')
+			{
 				st.push('(');
+			}
 			else
 			{
-				if(str.charAt(i-1)=='(')
+				if(arr[i-1] == '(') // 이전꺼가 ( 이면 하나 제거후 사이즈 넣기 (레이저쏘기)
 				{
 					st.pop();
 					answer += st.size();
-					
 				}
 				else
 				{
-					st.pop();
 					answer++;
+					st.pop();
 				}
 			}
 		}
 		
 		System.out.println(answer);
 		
-		
 	}
-	
 }
+
+// toCharArray() 를 사용하여 새로풀어봤는데 150ms 만큼 더걸린다..? // 다시해보니 오류였네 같은코드가 채점 섭컴터상태에따라 다른듯
