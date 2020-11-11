4277bc24686c3823569515fc6cef727d1a87fe9f
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-11 12:24:11 +0900

    프로그래머스 레벨2 올바른 괄호(R)

--- Coding_Test/src/programmers_level_2/Level2_07.java
+++ Coding_Test/src/programmers_level_2/Level2_07.java
@@ -1,55 +1,60 @@
-// Date : 2020.09.08
+// Date : 2020.09.08, 20.11.11
 // Title : 올바른 괄호
 // Author : Choi Cheol Nyeong
 // Language : Java
-// Classfication : 
- 
+// Classfication : Stack
+
 package programmers_level_2;
+
 import java.util.*;
 
 public class Level2_07
 {
-	
+
 	public static void main(String[] args)
 	{
 		Solution2_07 solution2_07 = new Solution2_07();
-		String [] str = {"(())","((((","))))","()()(())"};
-		
-		for(int i=0; i<str.length; i++)
+		String[] str =
+		{ "(())", "((((", "))))", "()()(())" };
+
+		for (int i = 0; i < str.length; i++)
 		{
 			System.out.println(solution2_07.solution(str[i]));
 		}
 	}
-}	
-	 class Solution2_07 {
-	    boolean solution(String s) {
-	        boolean answer = true;
-	        
-	        Stack<Character> st = new Stack<>();
-	        
-	        for(int i=0; i<s.length(); i++)
-	        {
-	            char temp = s.charAt(i);
-	            if(temp=='(')
-	            {
-	                st.push(temp);
-	            }
-	            else
-	            {
-	                if(!st.isEmpty())
-	                {if(st.peek()=='(')
-	                    st.pop();
-	                }
-	                else
-	                    st.push(temp);
-	            }
-	        }
-	        
-	        if(!st.isEmpty())
-	            answer = false;
-	        
-	        
-	        return answer;
-	    }
+}
+
+class Solution2_07
+{
+	boolean solution(String s)
+	{
+		boolean answer = true;
+
+		Stack<Character> st = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			int temp = s.charAt(i);
+
+			if (temp == '(')
+				st.push('(');
+			else
+			{
+				if (st.isEmpty())
+				{
+					return false;
+				} 
+				else
+				{
+					st.pop();
+				}
+			}
+
+		}
+
+		if (!st.isEmpty())
+			return false;
+
+		return answer;
 	}
-	
+}
