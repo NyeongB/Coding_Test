f476739e770b5478b8fd63aaad7b0511c24b4741
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-13 18:07:17 +0900

    백준 정렬 좌표 정렬하기(R)

--- Coding_Test/src/BOJ_Sorting/BOJ_Sorting_02.java
+++ Coding_Test/src/BOJ_Sorting/BOJ_Sorting_02.java
@@ -1,4 +1,4 @@
-// Date : 2020.09.01
+// Date : 2020.09.01, 20.11.13
 // Title : 좌표 정렬
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -6,13 +6,66 @@
 
 package BOJ_Sorting;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
 public class BOJ_Sorting_02
 {
+	public static void main(String[] agrs) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(br.readLine());
+		
+		int [][] arr = new int[num][2];
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			String [] temp = br.readLine().split(" ");
+			arr[i][0] = Integer.parseInt(temp[0]);
+			arr[i][1] = Integer.parseInt(temp[1]);
+		}
+		
+		Arrays.sort(arr, new Comparator<int []>() {
+			
+			public int compare(int [] a, int [] b)
+			{
+				
+				if(a[0] > b[0] )
+					return 1;
+				else if(a[0] < b[0])
+					return -1;
+				else
+				{
+					if(a[1] > b[1])
+						return 1;
+					else
+						return -1;
+				}
+				
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			sb.append(arr[i][0]+" "+arr[i][1]+"\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+		
+
+	}
+// 기존에는 class 를 만들어 값을 담았는데 int형 2차원 배열로 받아서 정렬하는게 제일 빠르다 
+	/*
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
@@ -79,5 +132,5 @@ class XY
 	{
 		return this.y;
 	}
-	
+	*/
 }
