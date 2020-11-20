f759954eb6fd81a9bdda0b0982fc310155bb3b4b
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-20 13:14:11 +0900

    프로그래머스 레벨2 큰 수 만들기(R)

--- Coding_Test/src/programmers_level_2/Level2_01.java
+++ Coding_Test/src/programmers_level_2/Level2_01.java
@@ -1,70 +1,57 @@
-// Date : 2020.08.22
+// Date : 2020.08.22, 20.11.20
 // Title : 큰 수 만들기
 // Author : Choi Cheol Nyeong
- 
-package programmers_level_2;
 
-import java.util.ArrayList;
-import java.util.List;
+package programmers_level_2;
 
 public class Level2_01
 {
 	public static void main(String[] args)
 	{
-		String test1 = "1924";	// k 2
-		String test2 = "1231234";	// k 3
+		String test1 = "1924"; // k 2
+		String test2 = "1231234"; // k 3
 		char ch = '9';
-		//test1.replace(0, 0);
-		//System.out.println(ch-'0');
+		// test1.replace(0, 0);
+		// System.out.println(ch-'0');
 		Solution solution = new Solution();
 		System.out.println(solution.solution(test2, 3));
 	}
-	
+
 }
 
+class Solution
+{
+	public String solution(String number, int k)
+	{
+		StringBuilder answer = new StringBuilder();	// 속도 줄이려고 별에별걸 다했는데 이게 제일 빠르다
+		
+		int len = number.length();
+		
+		int r_len = len - k;
+		
+		int s = -1;
+		
+		for (int i = 0; i < r_len; i++)
+		{
+			char max = '0';
+			for (int j = s + 1; j < len - r_len + 1 + i; j++)
+			{
+				char num = number.charAt(j);
+				if (max < num)
+				{
+					max = num;
+					s = j;
+				}
+			}
 
-class Solution {
-    public String solution(String number, int k) {
-        String answer = "";
-        int len = number.length();
-        int n = len-k;
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i=n; i>0; i--)
-        {
-            int max = 0;
-            int index=0;
-            String temp="";
-            for(int j=0; j<len-i+1; j++)
-            {
-                if(max<number.charAt(j)-'0')
-                {
-                	max = number.charAt(j)-'0';
-                    index = j;
-                }
-            }
-            //System.out.println(number);
-           for(int j=0; j<=index; j++)
-           {
-        	   temp +=0;
-        	//  number = number.replace(number.charAt(j), '0');
-           }
-           for(int j=index+1; j<len; j++)
-           {
-        	   temp +=number.charAt(j);
-           }
-           number = temp;
-          
-           list.add(max);
-          // System.out.printf("%d번째 맥스값 %d\n",i,max);
-        }
-        
-        for(int i=0; i<list.size(); i++)
-        {
-        	//System.out.println(list.get(i));
-        	answer +=list.get(i);
-        }
-        //System.out.println(answer);
-        return answer;
-    }
+			answer.append(max); // 속도가 100배 이상 빨라짐
+
+		}
+
+		return answer.toString();
+	}
 }
-
+// 리턴해야되는 길이가 4라면 
+// 맨뒤에 3자리는 제외한 값에서 찾아야하고 
+// 그다음은 2자리 .. 1자리 0자리.. 를 제외하고 찾는데
+// 대신 맥스값으로 선택된 값 뒤에있는 값부터 탐색해야한다.
