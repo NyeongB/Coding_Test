eb1cdc6cc15e249ae44947b4670e2593416edb96
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-27 21:02:08 +0900

    프로그래머스 레벨3 단속카메라(R)

--- Coding_Test/src/programmers_level_3/Level3_10.java
+++ Coding_Test/src/programmers_level_3/Level3_10.java
@@ -1,4 +1,4 @@
-// Date : 2020.11.04
+// Date : 2020.11.04, 20.11.27
 // Title : 단속카메라
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -29,9 +29,57 @@ public class Level3_10
 
 }
 
-class Solution3_10
+class Solution3_10	// 스태틱 클래스 안쓰고했는데 오히려 가독성 떨짐 구조체를 쓰는게 좋을듯
 {
-
+	 public int solution(int [][] r) {
+	        int answer = 1;
+	        
+	        Integer [][] routes = new Integer[r.length][2];
+	        
+	        for(int i=0; i<r.length; i++)
+	        {
+	            routes[i][0] = r[i][0];
+	            routes[i][1] = r[i][1];
+	        }
+	        
+	        Arrays.sort(routes, new Comparator<Integer []>(){
+	           
+	            public int compare(Integer [] a, Integer []  b)
+	            {
+	                if(a[0] > b[0])
+	                    return 1;
+	                else
+	                    return -1;
+	                
+	            }
+	        });
+	        
+	        
+	        Integer [] temp = new Integer[2];
+	        
+	        temp[0] = routes[0][0];
+	        temp[1] = routes[0][1];
+	        
+	        for(int i=1; i<routes.length; i++)
+	        {
+	            if(temp[1] > routes[i][1])
+	            {
+	                temp[0] = routes[i][0];
+	                temp[1] = routes[i][1];
+	            }
+	            else if(temp[1] < routes[i][0])
+	            {
+	                answer++;
+	                temp[0] = routes[i][0];
+	                temp[1] = routes[i][1];
+	                
+	            }
+	        }
+	        
+	        
+	        return answer;
+	    }
+	/*
 	static class Car
 	{
 		int s;
@@ -98,6 +146,7 @@ class Solution3_10
 
 		return count;
 	}
+	*/
 
 }
 //처음엔 정렬을하고 단순히 끝부분이 새로 들어오는거의 시작보다 크면 포함이고 작거나같으면 포함이 아니란 식으로 
