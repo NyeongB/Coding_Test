d964c85530ac9db11bfd99f69186d0233a0305cf
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-28 17:12:24 +0900

    프로그래머스 레벨2 땅따먹기(R)

--- Coding_Test/src/programmers_level_2/Level2_26.java
+++ Coding_Test/src/programmers_level_2/Level2_26.java
@@ -1,8 +1,8 @@
-// Date : 2020.11.06
+// Date : 2020.11.06, 20.11.28
 // Title : 땅따먹기
 // Author : Choi Cheol Nyeong
 // Language : Java
-// Classfication : 
+// Classfication : DP
 
 package programmers_level_2;
 
@@ -49,5 +49,33 @@ class Solution2_26
 	        
 	        return answer;
 	    }
-	}
+	 
+	 
+	 /*
+	  int solution(int[][] land) {	// 다시풀어봤는데 살짝 변형됨
+        int answer = 0;
+
+        int len = land.length;
+        
+        for(int i=0; i<len-1; i++)
+        {
+            land[i+1][0] = land[i+1][0] +
+                Math.max(land[i][1],Math.max(land[i][2],land[i][3]));
+            
+            land[i+1][1] = land[i+1][1] +
+                Math.max(land[i][0],Math.max(land[i][2],land[i][3]));
+            
+            land[i+1][2] = land[i+1][2] +
+                Math.max(land[i][1],Math.max(land[i][0],land[i][3]));
+            
+            land[i+1][3] = land[i+1][3] +
+                Math.max(land[i][1],Math.max(land[i][2],land[i][0]));
+        }
+
+        answer = Math.max(Math.max(Math.max(land[len-1][0],land[len-1][1]),land[len-1][2]),land[len-1][3]);
+        
+        return answer;
+    }
+	 */
+}
 // 컨셉은 자기자신을 제외한값의 맥스값과 자기자신과 N+1까지 쌓는것
