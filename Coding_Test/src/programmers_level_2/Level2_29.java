adc717ebd6f6f8dda58baa0613884c67c55a6df4
NyeongB <kdtoaj14@ajou.ac.kr>
2020-11-29 20:37:41 +0900

    프로그래머스 레벨2 JadenCase 문자열(R)

--- Coding_Test/src/programmers_level_2/Level2_29.java
+++ Coding_Test/src/programmers_level_2/Level2_29.java
@@ -1,8 +1,8 @@
-// Date : 2020.11.10
-// Title : 조이스틱
+// Date : 2020.11.10, 20.11.29
+// Title : JadenCase 문자열
 // Author : Choi Cheol Nyeong
 // Language : Java
-// Classfication : 그리디
+// Classfication : String
 
 package programmers_level_2;
 
@@ -20,36 +20,33 @@ public class Level2_29
 
 class Solution2_29
 {
-	public String solution(String s)
-	{
-		String answer = "";
-		s = s.toLowerCase(); // 모두 소문자로 만들고
-		boolean flag = true; // 공백 만날때마다 그 다음은 대문자를 위함
-
-		for (int i = 0; i < s.length(); i++)
-		{
-			char temp = s.charAt(i);
-
-			if (temp == ' ')
-			{
-				answer += ' ';
-				flag = true;
-				continue;
-			}
-
-			if (flag)
-			{
-				if (!Character.isDigit(temp))
-					temp = (char) (temp - 32);
-
-				flag = false;
-			}
-
-			answer += temp;
-		}
-
-		return answer;
-	}
+	 public String solution(String s) {
+	        String answer = "";
+	        boolean flag = true;    // 첫번째 대문자로 변경하기위해 true로 넣고 ' '만날때 트루
+	        s = s.toLowerCase();
+	        
+	        for(int i=0; i<s.length(); i++)
+	        {
+	            char temp = s.charAt(i);
+	            if(temp==' ')
+	            {
+	                flag = true;
+	                answer += ' ';  // 공백을 만날때마다 다음 문자는 대문자가됨
+	                continue;
+	            }
+	            
+	            if(flag)
+	            {
+	                if(!Character.isDigit(temp))
+	                    temp = (char)(temp - 32);
+	                flag = false;   // 한번 대문자로 만들고 그다음부턴 공백만나기전까지 소문자
+	            }
+	            
+	            answer += temp;
+	        }
+	        
+	        return answer;
+	    }
 }
 
 // 처음에 매개변수로 받은 문자열에 스플릿으로 접근해봤는데 안됐다
