9aadbb7217573e73bf56996c560991137ac9b5aa
NyeongB <kdtoaj14@ajou.ac.kr>
2020-12-09 14:45:24 +0900

    백준 BFS 섬의 갯수

--- Coding_Test/src/BOJ_BFS/BOJ_BFS_02.java
+++ Coding_Test/src/BOJ_BFS/BOJ_BFS_02.java
@@ -1,4 +1,4 @@
-// Date : 2020.09.08
+// Date : 2020.09.08, 20.12.08
 // Title : 섬의 갯수
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -12,102 +12,107 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// 우선 BFS 구현해본다.
-
-class Node
-{
-	int x, y;
-
-	public Node(int x, int y)
-	{
-		this.x = x;
-		this.y = y;
-	}
-}
 
 public class BOJ_BFS_02
 {
-	static int[] dx =
-	{ -1, -1, 0, 1, 1, 1, 0, -1 }; // 12시 부터 시계방향으로
-	static int[] dy =
-	{ 0, 1, 1, 1, 0, -1, -1, -1 };
+	static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 }; // 12시 부터 시계방향으로
+	static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-	static int[][] arr; // 섬 매트릭스
-	static int[][] visit; //
+	static int [][] map;
+	static boolean [][] visited;
 
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		while (true)
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(true)
 		{
-			String[] str = br.readLine().split(" ");
-			int w = Integer.parseInt(str[0]);
-			int h = Integer.parseInt(str[1]);
-
-			if (w == 0 && h == 0)
+			int N = 0;
+			int M = 0;
+			int count = 0;
+			String [] str = br.readLine().split(" ");
+			N = Integer.parseInt(str[0]);
+			M = Integer.parseInt(str[1]);
+			
+			if(N ==0 && M == 0)
 				break;
-
-			arr = new int[h][w];
-
-			for (int i = 0; i < h; i++) // 초기화
+			map = new int[M][N];
+			visited = new boolean[M][N];
+			
+			
+			
+			for(int i=0; i<M; i++)
 			{
 				str = br.readLine().split(" ");
-				for (int j = 0; j < w; j++)
+				for(int j=0; j<N; j++)
 				{
-					arr[i][j] = Integer.parseInt(str[j]);
+					map[i][j] = Integer.parseInt(str[j]);
 				}
 			}
-
-			int count = 0;
-			visit = new int[h][w];
-
-			for (int i = 0; i < h; i++)
+			
+			
+			for(int i=0; i<M; i++)
 			{
-				
-				for (int j = 0; j < w; j++)
+				for(int j=0; j<N; j++)
 				{
-					if (arr[i][j] == 1 && visit[i][j] == 0)
-						bfs(h, w, i, j, ++count);
-				}
-			}
-
-			sb.append(count + "\n");
-
-		}
-
-		System.out.println(sb.toString());
-
-	}
-
-	static void bfs(int h, int w, int x, int y, int count)
-	{
-		LinkedList<Node> q = new LinkedList<Node>();
-		visit[x][y] = 1;
-		q.offer(new Node(x, y));
-
-		while (!q.isEmpty())
-		{
-			Node temp = q.poll();
-			x = temp.x;
-			y = temp.y;
-			for (int i = 0; i < dx.length; i++)
-			{
-				int a = dx[i] + x;
-				int b = dy[i] + y;
-				if (0 <= a && a < h && 0 <= b && b < w)
-				{
-					if (arr[a][b] == 1 && visit[a][b] == 0)
+					if(map[i][j] == 1 && visited[i][j] == false)
 					{
-						q.offer(new Node(a, b));
-						visit[a][b] = 1;
+						bfs(i,j);
+						count++;
 					}
 				}
-
 			}
+			
+			sb.append(count+"\n");
+			
+		}
+		
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	static void bfs(int a, int b)
+	{
+		Queue<int []> q = new LinkedList<int []>();
+		
+		
+		q.add(new int[] {a,b});
+		visited[a][b] = true;
+		
+		while(!q.isEmpty())
+		{
+			int [] temp = q.poll();
+			
+			
+			for(int i=0; i<8; i++)
+			{
+				int x = dx[i] + temp[0];
+				int y = dy[i] + temp[1];
+				
+				if(x>=0&&y>=0&&x<map.length&&y<map[0].length)
+				{
+					
+				
+				
+				if(map[x][y] == 1 && !visited[x][y])
+				{
+					q.add(new int[] {x,y});
+					visited[x][y] = true;
+				}
+				
+				}
+			}
+			
 		}
 	}
+
+	
+	
 
 }
 
 // 교훈 : 입력하는 반복문 고대로 가져다 쓰지 않기.. 오류 찾느라 엄청 오래걸림 
+// BFS로 다시 풀어봄
