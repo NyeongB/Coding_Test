94e224778bc225de8bb16fe95b9aac696657c571
NyeongB <kdtoaj14@ajou.ac.kr>
2020-12-24 20:40:14 +0900

    백준 백트래킹 N과 M(1)

--- Coding_Test/src/BOJ_Backtracking/BOJ_Backtracking_02.java
+++ Coding_Test/src/BOJ_Backtracking/BOJ_Backtracking_02.java
@@ -1,4 +1,4 @@
-// Date : 2020.10.05
+// Date : 2020.10.05, 20.12.24
 // Title : N과 M(1)
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -11,53 +11,57 @@ import java.io.InputStreamReader;
 
 public class BOJ_Backtracking_02
 {
-	static int [] arr;
-	static int [] output;
-	static int [] visit;
-	static int n,r;
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N, M;
+		
 		String [] str = br.readLine().split(" ");
-		n = Integer.parseInt(str[0]);
-		r = Integer.parseInt(str[1]);
-		arr = new int[n];
 		
-		for(int i=0; i<n; i++)
-		{
-			arr[i] = i + 1;
-		}
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
 		
-		output = new int[n];
-		visit = new int[n];
+		int [] arr = new int[N];	// 1,2,3..
 		
-		perm(0);
+		for(int i=0; i<N; i++)
+			arr[i] = i+1;
+		
+		int [] result = new int[M];	// 결과
+		StringBuilder sb = new StringBuilder(); // 결과 출력용
+		boolean [] visited = new boolean[N];	// 방문기록 
+		
+		permutation(0, arr, result, sb, visited, N, M);
+		
+		System.out.println(sb.toString());
 	}
 	
-	static void perm(int depth)
+	public static void permutation(int depth, int [] arr, int [] result, StringBuilder sb, boolean[] visited, int N, int M)
 	{
-		if(depth == r)
+		if(M == depth)
 		{
-			for(int i=0; i<r; i++)
+			for(int i=0; i<M; i++)
 			{
-				System.out.print(output[i]+" ");
+				sb.append(result[i]+" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
 		}
 		
-		
-		for(int i=0; i<n; i++)
+		for(int i=0; i<N; i++)
 		{
-			if(visit[i] == 0)
+			if(!visited[i])
 			{
-				visit[i] = 1;
-				output[depth] = arr[i];
-				perm(depth + 1);
-				visit[i] = 0;
+				visited[i] = true;	// 방문기록 
+				
+				result[depth] = arr[i];	// 현재 뎊스에 넣기
+				
+				permutation(depth + 1, arr, result, sb, visited, N, M);	// 뎊스 올리고 재귀
+				
+				visited[i] = false;	// 방문기록 해제
 			}
 		}
-		
 	}
+	
 }
 // nPr의 순열 문제이다
