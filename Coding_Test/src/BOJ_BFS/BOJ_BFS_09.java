11536b7a3aaa7175e081d5a5034e3960475e3c2a
NyeongB <kdtoaj14@ajou.ac.kr>
2021-01-03 18:47:27 +0900

    백준 BFS 연구소

--- Coding_Test/src/BOJ_BFS/BOJ_BFS_09.java
+++ Coding_Test/src/BOJ_BFS/BOJ_BFS_09.java
@@ -1,4 +1,4 @@
-// Date : 2020.10.16
+// Date : 2020.10.16, 21.01.03
 // Title : 연구소
 // Author : Choi Cheol Nyeong
 // Language : Java
@@ -6,7 +6,6 @@
 
 package BOJ_BFS;
 
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,142 +14,152 @@ import java.util.Queue;
 
 public class BOJ_BFS_09
 {
-	
-	static int [][] map;
-	static int [][] visit;
 	static int N, M;
-	static int [] dx = {1,-1,0,0};
-	static int [] dy = {0,0,1,-1};
-	static int max = 0;
-	
-	static int [][] mapCopy;
-	static int [][] visitCopy;
-	
+	static int[][] map;
+	static int[][] copy;
+
+	static int[] dx = { 0, 0, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0 };
+
+	static int result = 0;
+
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String [] str = br.readLine().split(" ");
-		
+		String[] str = br.readLine().split(" ");
+
 		N = Integer.parseInt(str[0]);
 		M = Integer.parseInt(str[1]);
-		
 		map = new int[N][M];
-		visit = new int[N][M];
-		mapCopy = new int[N][M];
-		visitCopy = new int[N][M];
-		
-		for(int i=0; i<N; i++)
+		copy = new int[N][M];
+
+		for (int i = 0; i < N; i++)
 		{
+
 			str = br.readLine().split(" ");
-			
-			for(int j=0; j<str.length; j++)
+			for (int j = 0; j < M; j++)
 			{
 				map[i][j] = Integer.parseInt(str[j]);
 			}
 		}
-		
-		
-		
-		
-		recursion(0);
-		System.out.println(max);
-		
+		wall(0);
+
+		System.out.println(result);
+
 	}
-	
-	public static void recursion(int count)
+
+	// 벽 쌓기
+	public static void wall(int count)
 	{
-		if(count==3)
+		if (count == 3)
 		{
-			// 벽을 세개 놓았을때 bfs 시작
-			// bfs가 바이러스상태로 바꾸니깐 복사해서 진행함.
-			for(int i=0; i<N; i++)
+			/*
+			 * for(int i=0; i<N; i++) { for(int j=0; j<M; j++) {
+			 * System.out.print(copy[i][j]+" "); } System.out.println(); }
+			 * System.out.println();
+			 */
+
+			// 이때 bfs로 바이러스 탐색
+
+			copy(); // 이떄 벽 3개 쌓은 케이스를 복사
+
+			for (int i = 0; i < N; i++)
 			{
-				
-				for(int j=0; j<M; j++)
+				for (int j = 0; j < M; j++)
 				{
-					mapCopy[i][j] = map[i][j];
-					visitCopy[i][j] = visit[i][j];
+					if (copy[i][j] == 2)
+					{
+						bfs(i, j);
+					}
 				}
 			}
-			
-			bfs();
+
+			result = Math.max(result, check());
+
 			return;
 		}
-		
-		for(int i=0; i<N; i++)
+
+		// 벽 쌓기
+
+		for (int i = 0; i < N; i++)
 		{
-			for(int j=0; j<M; j++)
+			for (int j = 0; j < M; j++)
 			{
-				if(visit[i][j]==0 && map[i][j]==0)
+				if (map[i][j] == 0)
 				{
-					visit[i][j] = 1;
 					map[i][j] = 1;
-					recursion(count + 1);
-					visit[i][j] = 0;	// 다시 되돌리기
+					wall(count + 1);
 					map[i][j] = 0;
 				}
 			}
 		}
+
 	}
-	
-	
-	public static void bfs()
+
+	// 연구소 카피
+	public static void copy()
 	{
-		Queue<Point> q = new LinkedList<Point>();
-		
-		// 전체를 돌면서 2가 있나 확인
-		for(int i=0; i<N; i++)
+		for (int i = 0; i < N; i++)
 		{
-			for(int j=0; j<M; j++)
+			for (int j = 0; j < M; j++)
 			{
-				if(mapCopy[i][j]==2)
-				{
-					q.add(new Point(i,j));
-				}
+				copy[i][j] = map[i][j];
+				// copy_visited[i][j] = visited[i][j];
 			}
 		}
-		
-		while(!q.isEmpty())
+	}
+
+	// 안전 영역 갯수 체크
+	public static int check()
+	{
+
+		int result = 0;
+
+		for (int i = 0; i < N; i++)
 		{
-			Point temp = q.poll();
-			
-			for(int i=0; i<4; i++)
+			for (int j = 0; j < M; j++)
 			{
-				int nx = temp.x + dx[i];
-				int ny = temp.y + dy[i];
-				
-				if(nx>=0&&ny>=0&&nx<N&&ny<M)
+				if (copy[i][j] == 0)
+					result++;
+			}
+		}
+
+		return result;
+
+	}
+
+
+	// 바이러스 증식
+	public static void bfs(int x, int y)
+	{
+		int[] node = new int[]
+		{ x, y };
+		Queue<int[]> q = new LinkedList<>();
+		q.add(node);
+
+		while (!q.isEmpty())
+		{
+			int[] temp = q.poll();
+
+			for (int i = 0; i < 4; i++)
+			{
+				int nx = dx[i] + temp[0];
+				int ny = dy[i] + temp[1];
+
+				if (nx >= 0 && nx < N && ny >= 0 && ny < M)
 				{
-					if(mapCopy[nx][ny]==0 && visitCopy[nx][ny]==0)
+					if (copy[nx][ny] == 0)
 					{
-						mapCopy[nx][ny] = 2;
-						visitCopy[nx][ny] = 1;
-						q.add(new Point(nx,ny));
+						copy[nx][ny] = 2; // 바이러스 전이
+						q.add(new int[]
+						{ nx, ny });
 					}
 				}
 			}
 		}
-		
-		// 다 돌린후 안전영역의 갯수 체크
-		int cnt = 0;
-		for(int i=0; i<N; i++)
-		{
-			for(int j=0; j<M; j++)
-			{
-				if(mapCopy[i][j]==0)
-				{
-					cnt++;
-				
-				}
-			}
-		}
-		
-		max = Math.max(cnt, max);
-		
 	}
 }
 
-// 우선 막힘없이 술술 풀리는데 
-// 1 1 1 을 세개 넣은 상태로 bfs 돌리기전에 map 자체를 복사해 두는 경우 처음엔 mapCopy = map 으로 진행하였는데
-// 이렇게 진행할경우 주소값자체가 복사되는 것이여서 하나하나 일일이 값을 대입해주는 쪽으로 방향을 바꿔보았다
-// 그결과 바로 문제 해결
+// 벽 3개 놓은것을 전체 탐색하는 브루트포스와
+// 그떄마다 맵을 카피
+// 그리고 바이러스 증식하는 bfs가 함께 필요한 문제
